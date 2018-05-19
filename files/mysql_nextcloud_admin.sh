dc32f5c9ee4247092a6fd33eea0744ecaba30ca6
Thomas Zander <thomas.e.zander@googlemail.com>
2018-05-19 09:42:08 +0200

    Use roles

--- files/mysql_nextcloud_admin.sh
+++ files/mysql_nextcloud_admin.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+set -e
+
 ROOTPASSWORD=$1
 NCUSERNAME=$2
 NCUSERPASSWORD=$3
+
+if [ "x$ROOTPASSWORD" = "x" ]; then exit 1; fi
+if [ "x$NCUSERNAME" = "x" ]; then exit 1; fi
+if [ "x$NCUSERPASSWORD" = "x" ]; then exit 1; fi
 
 mysql -u root -p$ROOTPASSWORD << EOF
 CREATE DATABASE nextcloud;
--- files/mysql_secure.sh
+++ files/mysql_secure.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+set -e
+
 PASSWORD=$1
+
+if [ "x$PASSWORD" = "x" ]; then exit 1; fi
 
 mysql -u root << EOF
 UPDATE mysql.user SET Password=PASSWORD('$PASSWORD') WHERE User='root';
